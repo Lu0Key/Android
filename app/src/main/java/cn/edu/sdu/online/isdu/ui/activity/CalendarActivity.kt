a158f69c3499bb2b61cc7c1ac09d862144abf719
daflyinbed <1677759063@qq.com>
2018-07-18 08:20:41 +0800

    7/18 校历页面返回按钮

--- app/src/main/java/cn/edu/sdu/online/isdu/ui/activity/CalendarActivity.kt
+++ app/src/main/java/cn/edu/sdu/online/isdu/ui/activity/CalendarActivity.kt
@@ -1,7 +1,9 @@
 package cn.edu.sdu.online.isdu.ui.activity
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import cn.edu.sdu.online.isdu.R
 import cn.edu.sdu.online.isdu.app.SlideActivity
 
@@ -15,10 +17,26 @@ import cn.edu.sdu.online.isdu.app.SlideActivity
  ****************************************************
  */
 
-class CalendarActivity : SlideActivity() {
+class CalendarActivity : SlideActivity(), View.OnClickListener {
+
+    private var btnBack: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+        initView()
     }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.btn_back -> {
+                finish()
+            }
+        }
+    }
+    private fun initView() {
+        btnBack=findViewById(R.id.btn_back)
+        btnBack!!.setOnClickListener(this)
+    }
+
 }
