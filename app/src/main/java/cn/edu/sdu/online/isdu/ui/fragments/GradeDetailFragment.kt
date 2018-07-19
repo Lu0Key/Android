22272bed11c8094cdba1b91d78329c949dfc19be
TYZ <tyzjslyg@126.com>
2018-07-19 17:39:48 +0800

    7/19修复成绩界面卡死bug

--- app/src/main/java/cn/edu/sdu/online/isdu/ui/fragments/GradeDetailFragment.kt
+++ app/src/main/java/cn/edu/sdu/online/isdu/ui/fragments/GradeDetailFragment.kt
@@ -1,17 +1,14 @@
 package cn.edu.sdu.online.isdu.ui.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 
 import cn.edu.sdu.online.isdu.R
 import cn.edu.sdu.online.isdu.bean.Grade
@@ -125,7 +122,6 @@ class GradeDetailFragment : Fragment() {
     private fun checkEducateEvaluate(){
         if (judge) {
             progressDialog!!.dismiss()
-            dialog!!.dismiss()
         }
         else {
             progressDialog!!.dismiss()
