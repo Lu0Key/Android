78a28a395d4bfcbfa4f2207418ee6752986fe110
Zsj <jshazsj@qq.com>
2018-07-19 10:28:42 +0800

    7/19，完成自习室查询

--- app/src/main/java/cn/edu/sdu/online/isdu/ui/activity/SchoolBusTableActivity.kt
+++ app/src/main/java/cn/edu/sdu/online/isdu/ui/activity/SchoolBusTableActivity.kt
@@ -8,12 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import cn.edu.sdu.online.isdu.R
 import cn.edu.sdu.online.isdu.app.SlideActivity
 import cn.edu.sdu.online.isdu.bean.Bus
+import cn.edu.sdu.online.isdu.net.ServerInfo
+import cn.edu.sdu.online.isdu.net.pack.NetworkAccess
+import cn.edu.sdu.online.isdu.util.Logger
 import kotlinx.android.synthetic.main.activity_school_bus_table.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import org.json.JSONArray
+import java.io.IOException
 import java.util.*
 
 /**
@@ -26,22 +33,27 @@ import java.util.*
  ****************************************************
  */
 
-class SchoolBusTable : SlideActivity() ,View.OnClickListener{
+class SchoolBusTableActivity : SlideActivity() ,View.OnClickListener{
 
     private var searchNum : Int ?= null
     private var fromP : Int ?= null
     private var toP : Int ?= null
     private var backBtn : ImageView ?= null
-    private var adapter: SchoolBusTable.MyAdapter? = null
+    private var adapter: SchoolBusTableActivity.MyAdapter? = null
     private var recyclerView: RecyclerView ?= null
     private val dataList: MutableList<Bus> = ArrayList()
     private var textView : TextView ?= null
+
+    private val xqName = arrayOf("","中心校区","洪家楼校区","趵突泉校区","软件园校区","兴隆山校区","千佛山校区")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_school_bus_table)
         initView()
+
+        getData()
         initRecyclerView()
+
     }
 
     /**
@@ -49,15 +61,59 @@ class SchoolBusTable : SlideActivity() ,View.OnClickListener{
      */
     private fun initView(){
         val intent = intent
-        searchNum=intent.getIntExtra("searchNum",0)
-        fromP=intent.getIntExtra("fromP",0)
-        toP=intent.getIntExtra("toP",0)
+        searchNum = intent.getIntExtra("searchNum",0)
+        fromP = intent.getIntExtra("fromP",0)
+        toP = intent.getIntExtra("toP",0)
 
         backBtn = btn_back
         recyclerView = recycler_view
         textView = tips
 
         backBtn!!.setOnClickListener(this)
+    }
+
+    private fun getData() {
+        NetworkAccess.buildRequest(ServerInfo.getSchoolBusUrl(xqName[fromP!!], xqName[toP!!], searchNum!!),
+                object : Callback {
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        Logger.log(e)
+                    }
+
+                    override fun onResponse(call: Call?, response: Response?) {
+                        try {
+                            val jsonString = response?.body()?.string()
+                            if (jsonString == null || jsonString == "[]") {
+                                runOnUiThread {
+
+                                    textView!!.visibility = View.VISIBLE
+
+                                    dataList.clear()
+                                    adapter?.notifyDataSetChanged()
+                                }
+                                return
+                            }
+                            val jsonArray = JSONArray(jsonString)
+
+                            dataList.clear()
+                            for (i in 0 until jsonArray.length()) {
+                                val obj = jsonArray.getJSONObject(i)
+
+                                val bus = Bus()
+                                bus.busFrom = obj.getString("s")
+                                bus.busTo = obj.getString("e")
+                                bus.busTime = obj.getString("t")
+                                bus.busPass = obj.getString("p")
+                                dataList.add(bus)
+                            }
+
+                            runOnUiThread {
+                                adapter?.notifyDataSetChanged()
+                            }
+                        } catch (e: Exception) {
+                            Logger.log(e)
+                        }
+                    }
+                })
     }
 
     override fun onClick(v: View?) {
@@ -72,7 +128,6 @@ class SchoolBusTable : SlideActivity() ,View.OnClickListener{
      * 初始化RecyclerView
      */
     private fun initRecyclerView() {
-
         adapter = MyAdapter(dataList, this)
         recyclerView!!.layoutManager = LinearLayoutManager(this)
         recyclerView!!.adapter = adapter
@@ -89,7 +144,16 @@ class SchoolBusTable : SlideActivity() ,View.OnClickListener{
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             if (dataList?.size != 0){
                 textView!!.visibility = View.GONE
+            } else {
+                textView!!.visibility = View.VISIBLE
             }
+
+            val item = dataList!![position]
+
+            holder.busTime.text = item.busTime
+            holder.busFrom.text = item.busFrom
+            holder.busTo.text = item.busTo
+            holder.busPass.text = item.busPass
 
         }
 
--- app/src/main/java/cn/edu/sdu/online/isdu/ui/activity/StudyRoomActivity.kt
+++ app/src/main/java/cn/edu/sdu/online/isdu/ui/activity/StudyRoomActivity.kt
@@ -1,8 +1,10 @@
 package cn.edu.sdu.online.isdu.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +13,18 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import cn.edu.sdu.online.isdu.R
 import cn.edu.sdu.online.isdu.app.SlideActivity
+import cn.edu.sdu.online.isdu.net.ServerInfo
+import cn.edu.sdu.online.isdu.net.pack.NetworkAccess
 import cn.edu.sdu.online.isdu.ui.design.button.RadioImageButton
 import cn.edu.sdu.online.isdu.ui.design.dialog.WeekDialog
+import cn.edu.sdu.online.isdu.util.DateCalculate
 import cn.edu.sdu.online.isdu.util.EnvVariables
+import cn.edu.sdu.online.isdu.util.Logger
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import org.json.JSONArray
+import java.io.IOException
 
 /**
  ****************************************************
@@ -28,6 +39,12 @@ import cn.edu.sdu.online.isdu.util.EnvVariables
 class StudyRoomActivity : SlideActivity(), View.OnClickListener {
 
     private var dayNames = listOf("一", "二", "三", "四", "五", "六", "日")
+
+    private var campuses = listOf("中心校区", "洪家楼校区", "趵突泉校区", "软件园校区", "兴隆山校区", "千佛山校区")
+    private val mDataList = ArrayList<String>()
+
+    private var adapter: MyAdapter? = null
+
     private var radioButtons = ArrayList<RadioImageButton>()
     private var selectLayout: LinearLayout? = null
     private var selectWeek: TextView? = null
@@ -36,6 +53,9 @@ class StudyRoomActivity : SlideActivity(), View.OnClickListener {
     private var days = ArrayList<Int>()
     private var btnBack: ImageView? = null
     private var recyclerView: RecyclerView? = null
+
+    private var currentCampus = -1
+    private var date = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,13 +90,18 @@ class StudyRoomActivity : SlideActivity(), View.OnClickListener {
         btnBack = findViewById(R.id.btn_back)
 
         btnBack!!.setOnClickListener(this)
-        for (i in 0 until 6) {
+        for (i in 0 until radioButtons.size) {
             radioButtons[i].setOnClickListener {
-                if (radioButtons[i].isSelected)
+                if (radioButtons[i].isSelected) {
                     cancelAllClick()
-                else {
+                    mDataList.clear()
+                    adapter!!.notifyDataSetChanged()
+                } else {
                     cancelAllClick()
                     radioButtons[i].isSelected = true
+
+                    currentCampus = i
+                    getData(campuses[i])
                 }
             }
         }
@@ -115,6 +140,11 @@ class StudyRoomActivity : SlideActivity(), View.OnClickListener {
             dialog.show()
             dialog.show()
         }
+
+        /************************************
+         * 初始化RecyclerView的Adapter
+         ************************************/
+        adapter = MyAdapter(mDataList)
     }
 
     private fun initSelection() {
@@ -125,16 +155,73 @@ class StudyRoomActivity : SlideActivity(), View.OnClickListener {
 
         days.clear()
         days.add(EnvVariables.getCurrentDay())
+
+        date = DateCalculate.Cal(EnvVariables.currentWeek.toLong(), EnvVariables.getCurrentDay().toLong())
     }
 
     private fun cancelAllClick() {
         for (i in 0 until 6)
             radioButtons[i].isSelected = false
+        currentCampus = -1
     }
 
     private fun updateData() {
         selectWeek!!.text = weeks[0].toString()
         selectDay!!.text = dayNames[days[0] - 1]
+
+        date = DateCalculate.Cal(weeks[0].toLong(), days[0].toLong())
+        Log.d("AAA", date)
+    }
+
+    /**
+     * 从服务器获取数据
+     */
+    private fun getData(campus: String) {
+        mDataList.clear()
+        when (campus) {
+            campuses[0] -> {
+                mDataList.add("中心公教楼")
+                mDataList.add("中心文史楼")
+                mDataList.add("中心理综楼")
+                mDataList.add("中心电教北楼")
+                mDataList.add("中心知新楼B座")
+                mDataList.add("中心董明珠楼")
+            }
+            campuses[1] -> {
+                mDataList.add("洪楼法学新楼")
+                mDataList.add("洪楼3号楼")
+                mDataList.add("洪楼6号楼")
+                mDataList.add("洪楼公教楼")
+                mDataList.add("洪楼物理楼")
+            }
+            campuses[2] -> {
+                mDataList.add("趵突泉2号楼")
+                mDataList.add("趵突泉8号楼")
+                mDataList.add("趵突泉9号楼")
+                mDataList.add("趵突泉图东")
+                mDataList.add("趵突泉图西")
+            }
+            campuses[3] -> {
+                mDataList.add("软件园1区")
+                mDataList.add("软件园4区")
+                mDataList.add("软件园5区")
+            }
+            campuses[4] -> {
+                mDataList.add("兴隆山群楼A座")
+                mDataList.add("兴隆山群楼B座")
+                mDataList.add("兴隆山群楼C座")
+                mDataList.add("兴隆山群楼D座")
+                mDataList.add("兴隆山群楼E座")
+                mDataList.add("兴隆山讲学堂")
+            }
+            campuses[5] -> {
+                mDataList.add("千佛山1号楼")
+                mDataList.add("千佛山5号楼")
+                mDataList.add("千佛山9号楼")
+                mDataList.add("千佛山10号楼")
+            }
+        }
+        adapter?.notifyDataSetChanged()
     }
 
     override fun onResume() {
@@ -142,17 +229,13 @@ class StudyRoomActivity : SlideActivity(), View.OnClickListener {
         EnvVariables.init(this)
     }
 
-    private fun getDataFromNet() {
-
-    }
 
     private fun updateRecyclerView() {
-        val adapter = MyAdapter(listOf("软件园1区", "软件园5区"))
         recyclerView!!.layoutManager = LinearLayoutManager(this)
         recyclerView!!.adapter = adapter
     }
 
-    class MyAdapter(list: List<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    inner class MyAdapter(list: List<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
         private var list = list
 
@@ -164,13 +247,16 @@ class StudyRoomActivity : SlideActivity(), View.OnClickListener {
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             holder.textView.text = list[position]
             holder.layout.setOnClickListener {
-
+                startActivity(Intent(this@StudyRoomActivity, StudyRoomTableActivity::class.java)
+                        .putExtra("building", mDataList[position])
+                        .putExtra("campus", campuses[currentCampus])
+                        .putExtra("date", date))
             }
         }
 
         override fun getItemCount(): Int = list.size
 
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             var textView: TextView = view.findViewById(R.id.study_room_name)
             var layout: View = view.findViewById(R.id.layout)
         }
