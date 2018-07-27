70d04a81010a3d3e9206d620b88be39fd2f25004
Zsj <jshazsj@qq.com>
2018-07-28 00:05:43 +0800

    1.0.0

--- app/src/main/java/cn/edu/sdu/online/isdu/app/BuildConfig.java
+++ app/src/main/java/cn/edu/sdu/online/isdu/app/BuildConfig.java
@@ -3,7 +3,7 @@ package cn.edu.sdu.online.isdu.app;
 public class BuildConfig {
 
     public static final int VERSION_CODE = 0; // 版本序号
-    public static final String VERSION_NAME = "1.0.0-alpha"; // 版本名称
+    public static final String VERSION_NAME = "1.0.0"; // 版本名称
     public static final boolean IS_TEST_VERSION = true; // 是否为测试版应用
 
 }
